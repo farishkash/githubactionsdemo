d84718664c6368a4b3547417a8241a5ac1abd5aa
Farish Kashefinejad <farish@The-Beast.local>
2024-11-07 18:42:50 -0800

    fixed tests to pass

--- math.test.js
+++ math.test.js
@@ -1,11 +1,11 @@
 const { add, subtract, multiply, divide } = require('./maths');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(5);
+  expect(add(1, 2)).toBe(3);
 });
 
 test('subtracts 5 - 3 to equal 2', () => {
-  expect(subtract(5, 3)).toBe(1);
+  expect(subtract(5, 3)).toBe(2);
 });
 
 test('multiplies 4 * 5 to equal 20', () => {
